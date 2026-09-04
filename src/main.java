5aea1dc7aead8cd6f96837112a7d5a0c59d65dba
Bailey Brouard <bb530@kent.ac.uk>
2026-09-04 11:53:37 +0100

    Adding to the main, testing if a new graph is created with nodes does it correctly output the connecting nodes and weight given a root node

---
+++ src/main.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Main{
+
+public static void main(String[] args) {
+    
+    Graph graph = new Graph();
+    graph.addEdge("A", "B", 4);
+    graph.addEdge("A", "C", 1);
+    graph.addEdge("C", "B", 1); //test data
+    graph.addEdge("B", "D", 1);
+    graph.addEdge("C", "D", 5);
+
+    List<Edge> testA = graph.getNeighbors("A"); //testing neighbours from A
+
+    for (Edge edge : testA) { //for every edge in testA
+        System.out.println("A -> " + edge.getDestinationId() + " weight: " + edge.getWeight()); //print each connecting node and its weight
+    }
+
+
+    }
+}
